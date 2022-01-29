c247064cfd37de83ee36c2dafad23f7ed01e21e4
Mahed <mahedyar@gmail.com>
2022-01-29 19:31:35 +0330

    MaterialUI added to Chubeharaj

---
+++ mythical-masterpiece/pages/components/chubeHaraj/ChubeHaraj.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function BoxSx() {
+  return (
+    <Box
+      sx={{
+        // width: 100%,
+        height: 300,
+        backgroundColor: '#0a3680', 
+        backgroundImage : url("../../assest/Images/ChubeHaraj/sale_bg.png") 
+        
+      }}
+    />
+  );
+}
