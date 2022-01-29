8a0ddbc7550e0aa2297c83396ee36041d7848539
Mahed <mahedyar@gmail.com>
2022-01-29 16:07:17 +0330

    MaterialUI added to Chubeharaj

---
+++ mythical-masterpiece/pages/components/chubeHaraj/ChubeHaraj.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+export default function BoxSx() {
+  return (
+    <Box
+      sx={{
+        // width: 100%,
+        height: 300,
+        backgroundColor: '#0a3680', 
+        // backgroundImage : url("https://static.timcheh.com/uploads/manual/images/homepage/sale_bg.png") 
+        
+      }}
+    />
+  );
+}
--- mythical-masterpiece/pages/index.js
+++ mythical-masterpiece/pages/index.js
@@ -1,7 +1,19 @@
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>
+import ChubeHaraj from "./components/chubeHaraj/ChubeHaraj"
 
 
 export default function Home() {
   return (
-<div>Hello World</div>
+    <>
+    
+    <ChubeHaraj/>
+    
+    
+    </>
+
+
   )
 }
