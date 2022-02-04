eb74485b4dfb783c3f1d3ca5d051df776774b80e
Mahed <mahedyar@gmail.com>
2022-02-04 20:55:55 +0330

    ProductBigDescription added

--- mythical-masterpiece/pages/index.js
+++ mythical-masterpiece/pages/index.js
@@ -1,7 +1,17 @@
+import { Container } from "@mui/material";
+import Box from '@mui/material/Box';
 
+import ProductBigDescription from "../components/productBigDescription/ProductBigDescription";
 
 export default function Home() {
   return (
-<div>Hello World</div>
-  )
+    <>
+      {/* <TitleBar/> */}
+      <Container>
+        <Box sx={{marginTop : "40px" , border : "2px solid #d9d9d9" , borderRadius : "15px"}}>
+          <ProductBigDescription />
+        </Box>
+      </Container>
+    </>
+  );
 }
