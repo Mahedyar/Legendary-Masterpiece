b6f2f06ad3d3b475e2eeff90b300b0620a8a92e8
kowsar <k.borzuei@gmail.com>
2022-02-05 20:26:50 +0330

    mediaFooter

---
+++ mythical-masterpiece/components/footer/MediaFooter.js
@@ -0,0 +1,12 @@
+import Footer from '../footer/Footer'
+import ResFooter from '../footer/'
+import useMediaQuery from '@mui/material/useMediaQuery';
+export default function MediaFooter(){
+    const matches = useMediaQuery('(min-width:769px)');
+    return(
+        <>
+            {matches && <Footer/>}
+            {!matches && <ResFooter/>}
+        </>
+    )
+}
