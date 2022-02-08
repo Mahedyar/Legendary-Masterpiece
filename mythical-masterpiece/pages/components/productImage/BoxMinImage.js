e2fa38e437f7d18ed138350d367b7b08e1f447fd
kowsar <k.borzuei@gmail.com>
2022-02-08 09:58:06 +0330

    2th commit ProductImage

--- mythical-masterpiece/pages/components/productImage/BoxMinImage.js
+++ mythical-masterpiece/pages/components/productImage/BoxMinImage.js
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import DataCard from '../productImage/DataCard'
-import lamp from '../../../assets/Images/ProductImage/lamp.jpg'
+import ModalImage from "./ModalImage/ModalImage";
+import {useState} from "react";
 
 
 function Item(props) {
@@ -26,8 +27,13 @@ function Item(props) {
     );
 }
 export default function BoxMinImage(){
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return(
         <>
+            {open && <ModalImage onClose={handleClose} open={open}/>}
             <div style={{ width: '100%' }}>
 
                 {DataCard.map((item,index)=><Box key={item.product}
@@ -39,9 +45,10 @@ export default function BoxMinImage(){
                         alignItems:'center',
                     }}>
 
-                        {item.images.map((img,index)=><Item key={item.product}> <Box key={`${item}-${img}`}>
-                            <img className={'Img-product'} width={40}height={40} src={img}/></Box></Item>)}
-                    <Item sx={{paddingTop:3.5,cursor:'pointer',color:'#595959'}}><Box>+{item.images.length+item.moreImage.length}</Box></Item>
+                        {item.images.map((img,index)=><Item onClick={handleOpen} key={item.product}><Box>
+                            <img  className={'Img-product'} width={40}height={40} src={img}/></Box></Item>)}
+                    <Item onClick={handleOpen}
+                          sx={{paddingTop:3.5,cursor:'pointer',color:'#595959'}}><Box>+{item.images.length+item.moreImage.length}</Box></Item>
                     </Box>)}
 
 
