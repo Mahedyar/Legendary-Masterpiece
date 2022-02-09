bb2353b6ca518353fb98cac04e86080e3866e53b
kowsar <k.borzuei@gmail.com>
2022-02-09 21:55:40 +0330

    edit ProductVizhegiPrice

--- mythical-masterpiece/pages/components/productVizhegiPrice/headerPrice/Color.js
+++ mythical-masterpiece/pages/components/productVizhegiPrice/headerPrice/Color.js
@@ -1,19 +1,15 @@
-
 import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
 import Box from "@mui/material/Box";
 import {useState} from "react";
 
 
-
 function Item(props) {
-    const { sx, ...other } = props;
+    const {sx, ...other} = props;
     return (
         <Box
             sx={{
-                paddingLeft:2,
-                fontSize: '0.875rem',
-                fontWeight: '700',
-                direction:'rtl',
+                m: 0.5,
+                direction: 'rtl',
                 ...sx,
             }}
             {...other}
@@ -22,19 +18,22 @@ function Item(props) {
 }
 
 export default function Color() {
-    const [select,setSelect]=useState({show:false,id:null})
-    const selector=(key)=>{
+    const [select, setSelect] = useState({show: false, id: null})
+    const selector = (key) => {
         setSelect({
             show: true,
-            id:key
+            id: key
         })
     }
 
-    const color = [{id: 1, color: '#feed00',name:'زرد'},{id:2,color: '#0000ff',name:'آبی'}, {id:3,color: '#000000',name:'مشکی'},{id:4,color: 'pink',name:'صورتی'}]
-    let outLinedIcon=<CheckOutlinedIcon/>
+    const color = [
+        {id: 1, color: '#feed00', name: 'زرد'}, {id: 2, color: '#0000ff', name: 'آبی'},
+        {id: 3, color: '#000000', name: 'مشکی'}, {id: 4, color: 'pink', name: 'صورتی'}
+    ]
+    let outLinedIcon = <CheckOutlinedIcon/>
     return (
         <>
-            <div >
+            <div>
 
                 <p>رنگ:{}</p>
                 <Box
@@ -42,14 +41,18 @@ export default function Color() {
                         display: 'flex',
                         flexDirection: 'row',
 
+
                     }}>
 
-                    {color.map((item)=> <Item className={`${select.show && select.id===item.id?'border':''}`} onClick={()=>selector(item.id)} key={item.id}>
+                    {color.map((item) => <Item className={`${select.show && select.id === item.id ? 'border' : ''}`}
+                                               onClick={() => selector(item.id)} key={item.id}>
 
-                        {select.show && select.id===item.id?
+                        {select.show && select.id === item.id ?
 
-                        <CheckOutlinedIcon sx={{color: '#fff', borderRadius: 1, background: item.color,fontSize:20}}/>:
-                            <CheckOutlinedIcon sx={{color: item.color, borderRadius: 1, background: item.color,fontSize:20}}/>}
+                            <CheckOutlinedIcon
+                                sx={{color: '#fff', borderRadius: 1, background: item.color, fontSize: 20}}/> :
+                            <CheckOutlinedIcon
+                                sx={{color: item.color, borderRadius: 1, background: item.color, fontSize: 20}}/>}
                     </Item>)}
                 </Box>
             </div>
--- mythical-masterpiece/pages/components/productVizhegiPrice/headerPrice/SubBottom.js
+++ mythical-masterpiece/pages/components/productVizhegiPrice/headerPrice/SubBottom.js
@@ -39,14 +39,14 @@ export default function subBottom(){
                         <Stack direction="row" spacing={1}>
                             <Button sx={{direction:"ltr"}} variant="contained" endIcon={<StarIcon sx={{fontSize:12}}/>}>4.9</Button>
                             <span/>
-                            <span style={{color: '#8c8c8c'}}> از 57 رای</span>
+                            <span style={{color: '#8c8c8c',fontSize:'12px'}}> از 57 رای</span>
                         </Stack>
                     </Item>
                     <Item>
                         <Stack direction="row" spacing={1}>
-                        <Button className={'btn--SubBottom'} sx={{direction:"ltr"}} variant="contained" endIcon={<TextsmsOutlinedIcon sx={{fontSize:12}}/>}></Button>
+                        <Button className={'btn--SubBottom'} sx={{direction:"ltr"}} variant="contained" endIcon={<TextsmsOutlinedIcon sx={{fontSize:10}}/>}></Button>
                             <span/>
-                            <span style={{color: '#8c8c8c'}}> (57 نظر)</span>
+                            <span style={{color: '#8c8c8c',fontSize:'12px'}}> (57 نظر)</span>
                     </Stack>
                     </Item>
                 </Box>
