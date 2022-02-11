c8d029b06cb72853079a47742b687241c1714450
kowsar <k.borzuei@gmail.com>
2022-02-11 14:14:58 +0330

    first commit create grid

---
+++ mythical-masterpiece/pages/productPage.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+
+export default function ProductPage() {
+    return (
+        <Container maxWidth="lg">
+        <Box
+            sx={{
+                width: '100%',
+                height: '140px',
+                color: '#fff',
+                '& > .MuiBox-root > .MuiBox-root': {
+                    p: 1,
+
+                    fontSize: '0.875rem',
+                    fontWeight: '700',
+                },
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(8, 1fr)',
+                    gap: 1,
+                    gridTemplateRows: 'auto',
+                    gridTemplateAreas: `"header header header header header header header header"
+                                         "breadCrumb breadCrumb breadCrumb breadCrumb breadCrumb breadCrumb breadCrumb breadCrumb"
+                                         ". . . . . . . . "
+                                         ". . . . . . . . "
+                                        "main main info info info sidebar sidebar sidebar "
+                                        "stylesFeatures stylesFeatures stylesFeatures stylesFeatures stylesFeatures stylesFeatures stylesFeatures stylesFeatures "
+                                         "tabList tabList tabList tabList  tabList tabList tabList tabList"
+                                         ". . . . . . . . "
+                                         ". . . . . . . . "
+                                         "footer footer footer footer  footer footer footer footer"`,
+                }}
+            >
+                <Box sx={{ gridArea: 'header', bgcolor: 'primary.main' }}>Header</Box>
+                <Box sx={{ gridArea: 'breadCrumb', bgcolor: 'pink' }}>breadCrumb</Box>
+                <Box sx={{ gridArea: 'main', bgcolor: 'secondary.main' }}>Main</Box>
+                <Box sx={{ gridArea: 'info', bgcolor: 'orange' }}>info</Box>
+                <Box sx={{ gridArea: 'sidebar', bgcolor: 'error.main' }}>Sidebar</Box>
+                <Box sx={{ gridArea: 'stylesFeatures', bgcolor: 'gold' }}>stylesFeatures</Box>
+                <Box sx={{ gridArea: 'tabList', bgcolor: '#20c02b' }}>tabList</Box>
+                <Box sx={{ gridArea: 'footer', bgcolor: 'warning.dark' }}>Footer</Box>
+
+            </Box>
+        </Box>
+        </Container>
+    );
+}
