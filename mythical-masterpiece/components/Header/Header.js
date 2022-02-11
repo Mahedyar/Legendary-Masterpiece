c112f26564898ec27bc4840705c08caf2c3b8f63
Mahed <mahedyar@gmail.com>
2022-02-11 18:56:30 +0330

    menu first parts added

--- mythical-masterpiece/components/Header/Header.js
+++ mythical-masterpiece/components/Header/Header.js
@@ -2,14 +2,16 @@ import { Fragment } from "react";
 import classes from "./Header.module.css";
 import HeaderGif from "../../assets/Images/Header/timche-header-add.gif";
 import TitlePart from "./TitlePart";
+import Menu from "./Menu"
 import { Container } from "@mui/material";
 
 const Header = () => {
   return (
     <>
-      <img className={classes.gifImage} src={HeaderGif.src} alt="some shit" />
+      <img className={classes.gifImage} src={HeaderGif.src} alt="someThing" />
       <Container sx={{marginTop : "15px"}}>
         <TitlePart />
+        <Menu/>
       </Container>
     </>
   );
