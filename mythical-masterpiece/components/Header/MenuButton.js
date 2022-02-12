cd003e14d3c8f4f9448b0b56159980689767ee39
Mahed <mahedyar@gmail.com>
2022-02-13 00:19:23 +0330

    MenuButton Show and 180 rotate Fixed

--- mythical-masterpiece/components/Header/MenuButton.js
+++ mythical-masterpiece/components/Header/MenuButton.js
@@ -1,61 +1,67 @@
-import Box from '@mui/material/Box';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Box from "@mui/material/Box";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ItemsMenu from "./ItemsMenu";
-import {useState} from "react";
+import { useState } from "react";
 import ModalMenu from "./ModalMenu";
 
-
 function Item(props) {
-    const { sx, ...other } = props;
-    return (
-        <Box
-            sx={{
-                p: 1,
-                m: 1,
-                fontSize: '0.8125rem',
-                ...sx,
-            }}
-            {...other}
-        />
-    );
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        p: 1,
+        m: 1,
+        fontSize: "0.8125rem",
+        ...sx,
+      }}
+      {...other}
+    />
+  );
 }
 
-export default function MenuButton(){
-    const [show,setShow]=useState({show:false,id:null})
-    const [open, setOpen] =useState({show: false,id:null});
-    const handleOpen = (key) => setOpen({show: true,id:key});
-    const handleClose = (key) => setOpen({show:false,id:key});
-    const hoverIcon=(key)=> setShow({show: true,id:key});
-    const unHoverIcon=(key)=>setShow({show: false,id:key})
+export default function MenuButton() {
+  const [show, setShow] = useState({ show: false, id: null });
+  const [open, setOpen] = useState({ show: false, id: null });
+  const handleOpen = (key) => setOpen({ show: true, id: key });
+  const handleClose = (key) => setOpen({ show: false, id: key });
+  const hoverIcon = (key) => setShow({ show: true, id: key });
+  const unHoverIcon = (key) => setShow({ show: false, id: key });
 
-
-
-
-
-    return(
-        <>
-            {open.show && <ModalMenu onConfirm={handleClose} onOpen={open}/>}
-            <div style={{ width: '100%' }}>
-                <Box
-
-                    onMouseLeave={unHoverIcon}
-
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        direction:'rtl',
-                    }}
-                >
-                    {ItemsMenu.map((item)=>
-                        <Item onMouseEnter={()=>hoverIcon(item.id)}  onMouseOver={handleOpen} className={'btn-menu'} key={item.id}>
-                            {item.title}{show.show && show.id===item.id?
-                            <ExpandMoreIcon className={'icon-menu'}  sx={{fontSize:10.5}}/>:
-                             <ExpandMoreIcon  sx={{fontSize:10.5}}/>}
-                        </Item>)}
-                </Box>
-
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      {open.show && <ModalMenu onClose={open.show} onOpen={open.show} />}
+      <div style={{ width: "100%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            direction: "rtl",
+          }}
+        >
+          {ItemsMenu.map((item) => (
+            <Item
+              onMouseEnter={() => hoverIcon(item.id)}
+              onMouseLeave={function (event) {
+                handleClose();
+                unHoverIcon();
+              }}
+              onMouseOver={handleOpen}
+              className={"btn-menu"}
+              key={item.id}
+            >
+              {item.title}
+              {show.show && show.id === item.id ? (
+                <ExpandMoreIcon
+                  className={"icon-menu"}
+                  sx={{ fontSize: 10.5 }}
+                />
+              ) : (
+                <ExpandMoreIcon sx={{ fontSize: 10.5 }} />
+              )}
+            </Item>
+          ))}
+        </Box>
+      </div>
+    </>
+  );
 }
--- mythical-masterpiece/components/Header/ModalMenu.js
+++ mythical-masterpiece/components/Header/ModalMenu.js
@@ -1,34 +1,27 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 
 const style = {
-    position: 'absolute',
-    top: '61%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '80%',
-    height:'70%',
-    bgcolor: '#fff',
-   outline:'none',
-    p: 3,
+  position: "absolute",
+  top: "61%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  height: "70%",
+  bgcolor: "#fff",
+  outline: "none",
+  p: 3,
 };
-export default function ModalMenu(props){
-
-
-    return(
-        <>
-
-            <Modal
-                open={props.onOpen}
-                   onClose={props.onConfirm}>
-                <Box sx={style} >
-                    <h1>hello</h1>
-                    <h1>hello</h1>
-
-
-                </Box>
-            </Modal>
-        </>
-    )
+export default function ModalMenu(props) {
+  return (
+    <>
+      <Modal sx={{backgroundColor : "red", position : "unset"}} open={props.onOpen} onClose={props.onConfirm}>
+        <Box sx={style}>
+          <h1>hello</h1>
+          <h1>hello</h1>
+        </Box>
+      </Modal>
+    </>
+  );
 }
