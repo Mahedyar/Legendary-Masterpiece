1344f43e0a096e922240107efa8355243c4809f7
Mahed <mahedyar@gmail.com>
2022-02-13 21:25:02 +0330

    Fonts and NumberDots added

--- mythical-masterpiece/components/chubeHaraj/ItemCard.js
+++ mythical-masterpiece/components/chubeHaraj/ItemCard.js
@@ -14,7 +14,9 @@ function numberToPersian(number) {
     6: "۶",
     7: "۷",
     8: "۸",
-    9: "۹",
+    9: "۹", 
+    "," : "," ,
+    
   };
   number = number.toString().split("");
   let persianNumber = "";
@@ -28,10 +30,15 @@ function numberToPersian(number) {
 }
 
 export default function ItemCard(props) {
+  const numberDotmaker = (number) => {
+    var parts = number.toString().split(".");
+        parts[0]=parts[0].replace(/\B(?=(\d{3})+(?!\d))/g,",");
+        return parts.join(",");
+  }
   const finalPrice = () => {
-    return numberToPersian(
-      (props.product.price * (100 - props.product.offPercent)) / 100
-    );
+    return numberToPersian(numberDotmaker((props.product.price * (100 - props.product.offPercent)) / 100))
+      
+    
   };
 
   return (
@@ -41,13 +48,6 @@ export default function ItemCard(props) {
         height: props.fullSize ? 388 : 403 ,
         width:  props.fullSize ? 215 : 240 ,
         boxSizing: "border-box",
-        // marginLeft: 2,
-        // paddingTop: props.cardsPaddingTop,
-        // padding : "16px" ,
-        // marginBottom: 3,
-       
-       
-        // minWidth : "138px"
       }}
     >
       <ItemImage image={props.product.image} />
@@ -71,7 +71,7 @@ export default function ItemCard(props) {
             marginRight: 1,
           }}
         >
-          {`%${numberToPersian(props.product.offPercent)}`}
+          {`%${numberToPersian(props.product.offPercent) }`}
         </Typography>
         <Typography
           component="span"
@@ -80,7 +80,7 @@ export default function ItemCard(props) {
             color: "gray",
           }}
         >
-          {numberToPersian(props.product.price)}
+          {numberToPersian(numberDotmaker(props.product.price))}
         </Typography>
       </Typography>
       <Typography
