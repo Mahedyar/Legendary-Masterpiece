dab5d525954449692f3e5ff5d27b9cc8fae2bbc1
mahdi fazli <mahdifazli5459@gmail.com>
2022-03-06 11:53:51 +0330

    added server to the home page

--- mythical-masterpiece/pages/index.js
+++ mythical-masterpiece/pages/index.js
@@ -1,10 +1,32 @@
 import HomePageComponent from "../components/homePage/HomePageComponent";
 import {products, productCards} from "../data/products";
+import {MongoClient} from "mongodb";
 
-export default function HomePage() {
-    // const cards = productCards.json();
-    // console.log(cards)
+export default function HomePage(props) {
     return (
-        <HomePageComponent products={products}/>
+        <HomePageComponent products={props.products}/>
     );
+}
+
+export const getStaticProps = async () => {
+    const client = await MongoClient.connect(
+        "mongodb+srv://mahdi:MdFfxuJC78ietT@daneshkarcluster.xri1m.mongodb.net/ShoppingItemsDatabase?retryWrites=true&w=majority"
+    )
+
+    const db = client.db();
+
+    const productsCollection = db.collection("ShopItem");
+    const cards = await productsCollection.find().toArray();
+    client.close();
+    return {
+        props: {
+            products: cards.map(item => ({
+                id: item._id.toString(),
+                image: item.image[0],
+                name: item.name,
+                offPercent: item.offPercent,
+                price: item.price,
+            }))
+        }
+    }
 }
